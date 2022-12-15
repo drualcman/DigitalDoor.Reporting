c9330b68ace4df1141d055af211f0be229d563c3
Royette Millares <royettemillares.ifl@gmail.com>
2022-12-15 15:57:53 +0800

    Done with all the pages

---
+++ DigitalDoor.Reporting.Website/Pages/Wiki.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DigitalDoor.Reporting.Website.Pages
+{
+    public partial class Wiki
+    {
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+        IJSObjectReference JSObjectReference { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstTime)
+        {
+            JSObjectReference = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "/js/app.js");
+            await JSObjectReference.InvokeVoidAsync("TabsSlide");
+        }
+    }
+}
