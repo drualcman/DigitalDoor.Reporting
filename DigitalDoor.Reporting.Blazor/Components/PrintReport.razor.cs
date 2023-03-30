f61405365ec0dc95218f4670089c3af3ca29f301
Sergi Ortiz Gómez <drualcman@msn.com>
2023-03-31 07:01:23 +0800

    removed itext7.html2pdf

--- DigitalDoor.Reporting.Blazor/Components/PrintReport.razor.cs
+++ DigitalDoor.Reporting.Blazor/Components/PrintReport.razor.cs
@@ -1,14 +1,9 @@
 ﻿using DigitalDoor.Reporting.Blazor.Models;
+using DigitalDoor.Reporting.Entities.Helpers;
 using DigitalDoor.Reporting.Entities.ValueObjects;
 using DigitalDoor.Reporting.Entities.ViewModels;
-using iText.Commons.Actions.Contexts;
-using iText.Html2pdf;
-using iText.Html2pdf.Attach;
-using iText.StyledXmlParser.Node;
-using iText.StyledXmlParser.Resolver.Resource;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using System.Text;
 
 namespace DigitalDoor.Reporting.Blazor.Components
 {
@@ -59,26 +54,13 @@ namespace DigitalDoor.Reporting.Blazor.Components
             PdfResponse response = await DocumentBuilder.GetHtml();
             try
             {
-                //string pageOrientation = ReportModel.Page.Orientation == Orientation.Landscape ? "l" : "p";
+                string pageOrientation = ReportModel.Page.Orientation == Orientation.Landscape ? "l" : "p";
 
-                //PageDimension = ReportModel.Page.Dimension;
-                //ReportFunctions reportFunctions = new ReportFunctions();
-                //string paperSize = reportFunctions.GetPaperSizeName(PageDimension).ToLower();
-                //await using IJSObjectReference JSModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/DigitalDoor.Reporting.Blazor/Printing-Report.js");
-                //response = await JSModule.InvokeAsync<PdfResponse>("PrintReports.CreatePdf", WrapperId, pdfName, !DirectDownload, pageOrientation, paperSize);
-
-                response.Html += $"<link href=\"{NavigationManager.BaseUri}_content/DigitalDoor.Reporting.Blazor/DigitalDoor.Reporting.Blazor.bundle.scp.css\" rel=\"stylesheet\" type=\"text/css\" />";
-
-                MemoryStream pdf = new MemoryStream(response.Html.Length);
-                ConverterProperties properties = new ConverterProperties();
-                properties.SetBaseUri(NavigationManager.BaseUri);
-                properties.SetImmediateFlush(true);
-                
-                HtmlConverter.ConvertToPdf(response.Html, pdf, properties);
-                
-                byte[] bytes = pdf.ToArray();
-                response.Base64String = Convert.ToBase64String(bytes);
-
+                PageDimension = ReportModel.Page.Dimension;
+                ReportFunctions reportFunctions = new ReportFunctions();
+                string paperSize = reportFunctions.GetPaperSizeName(PageDimension).ToLower();
+                await using IJSObjectReference JSModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/DigitalDoor.Reporting.Blazor/Printing-Report.js");
+                response = await JSModule.InvokeAsync<PdfResponse>("PrintReports.CreatePdf", WrapperId, pdfName, !DirectDownload, pageOrientation, paperSize);
             }
             catch(Exception ex)
             {
@@ -108,12 +90,4 @@ namespace DigitalDoor.Reporting.Blazor.Components
 
         Task GeneratePdf() => GeneratePdf(string.IsNullOrEmpty(PdfName) ? "document.pdf" : PdfName);
     }
-}
-
-class Resources : IResourceRetriever
-{
-
-    //_content/DigitalDoor.Reporting.Blazor/DigitalDoor.Reporting.Blazor.bundle.scp.css
-    public byte[] GetByteArrayByUrl(Uri url) => throw new NotImplementedException();
-    public Stream GetInputStreamByUrl(Uri url) => throw new NotImplementedException();
 }
