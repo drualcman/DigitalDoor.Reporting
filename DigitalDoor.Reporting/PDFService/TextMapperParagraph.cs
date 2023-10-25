f63fe330b688fd432fab6420bc8221367cb1a016
Sergi Ortiz Gómez <drualcman@msn.com>
2023-10-25 08:38:56 +0800

    add height and width to the textparagraph

--- DigitalDoor.Reporting/PDFService/TextMapperParagraph.cs
+++ DigitalDoor.Reporting/PDFService/TextMapperParagraph.cs
@@ -12,7 +12,7 @@ namespace DigitalDoor.Reporting.PDFService;
 
 internal class TextMapperParagraph : TextMapperBase
 {
-    public Paragraph SetParagraph(string textValue, ColumnContent item, decimal height, decimal weight)
+    public Paragraph SetParagraph(string textValue, ColumnContent item, decimal height, decimal width)
     {
         Paragraph Text = new Paragraph();
         Text.SetFontSize((float)(item.Column.Format.FontDetails.ColorSize.Width));
@@ -83,17 +83,21 @@ internal class TextMapperParagraph : TextMapperBase
             Report.TextAlignment.Justify => TextAlignment.JUSTIFIED,
             _ => TextAlignment.LEFT,
         };
+        
+        Text.SetHeight(MillimeterToPixel(height));
+        Text.SetWidth(MillimeterToPixel(width));
+
         Text.SetTextAlignment(Aligment);
         if (item.Column.Format.Angle != 0)
         {
-            Text.SetFixedPosition(MillimeterToPixel(item.Column.Format.Position.Left+weight)+MillimeterToPixel(item.Column.Format.Dimension.Width/1.49),
+            Text.SetFixedPosition(MillimeterToPixel(item.Column.Format.Position.Left+width)+MillimeterToPixel(item.Column.Format.Dimension.Width/1.49),
             MillimeterToPixel(height-(item.Column.Format.Position.Top+10)),
                                   MillimeterToPixel(item.Column.Format.Dimension.Width));
             Text.SetRotationAngle(ConvertAngleToRadian(item.Column.Format.Angle));
         }
         else
         {
-            Text.SetFixedPosition(MillimeterToPixel(item.Column.Format.Position.Left+weight),
+            Text.SetFixedPosition(MillimeterToPixel(item.Column.Format.Position.Left+width),
                                   MillimeterToPixel(height-(item.Column.Format.Position.Top+(decimal)(item.Column.Format.FontDetails.ColorSize.Width*0.53))),
                                   MillimeterToPixel(item.Column.Format.Dimension.Width));
         }
