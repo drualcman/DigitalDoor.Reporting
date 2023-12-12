a32ade2e347ad11fe20e398f5c830ba0ef5ccf63
Sergi Ortiz Gómez <drualcman@msn.com>
2023-12-12 14:37:35 +0800

    refactor to avoid duplicate codes

---
+++ DigitalDoor.Reporting.Blazor/Helpers/LoadJsModuleHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace DigitalDoor.Reporting.Blazor.Helpers;
+internal static class LoadJsModuleHelper
+{        
+    internal static Task<IJSObjectReference> GetJSObjectReference(IJSRuntime jsRuntime, string filename) =>
+        jsRuntime.InvokeAsync<IJSObjectReference>(
+            "import", $"./{ContentHelper.ContentPath}/{filename}?v={DateTime.Today.ToFileTimeUtc()}").AsTask();
+
+}
